e1d2f1fe7b4d82bb7fd818df3b677060237b3cbe
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-05-31 13:01:04 +0430

    Creates Big picture of program

---
+++ app/BinarySearch.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App;
+
+
+class BinarySearch
+{
+    private array $dataset;
+
+    public function GetArray(array $arr):void
+    {
+        $this->dataset = $arr;
+    }
+
+    public function Find(int $keyword):bool
+    {
+        return false;
+    }
+
+}
--- app/Kernel.php
+++ app/Kernel.php
@@ -7,6 +7,15 @@ namespace App;
 class Kernel
 {
     public function run(){
-        echo 'Poilerplate is ready.';
+        $keyword = 5;
+        $arr = [1,2,3,4,5,6,7,8,9];
+
+        $search_engine = new BinarySearch();
+        $search_engine->GetArray($arr);
+        $result = $search_engine->Find($keyword);
+
+        $result ?
+            printf("%d is in the Array",$keyword)
+            : printf("%d is not in the Array",$keyword);
     }
 }
